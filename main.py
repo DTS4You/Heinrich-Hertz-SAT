1d74b1c8af7240a01a4c7714212b1ce5e2bc64c4
Norbert Schwarz <info@datentechnikschwarz.de>
2024-05-30 14:55:10 +0200

    Init

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 ######################################################
 ### Main-Program                                   ###
 ### Projekt: Heinrich-Hertz-SAT                    ###
-### Version: 1.03        30.04.2024                ###
+### Version: 1.04        30.05.2024                ###
 ######################################################
 from machine import Pin, Timer # type: ignore
 from module_init import Global_Module as MyModule
